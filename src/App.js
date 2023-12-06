dc47e238739962aa7789b4cd452fd47ce1996933
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-12-06 23:38:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import StadiumList from './Component/StadiumList';
+
+function App() {
+  return (
+    <StadiumList/>
+  );
+}
+
+export default App;
