1b36724e31a7972828cffb976284cb1270935407
BouchraBenGhazala <ghazalabouchra@gmail.com>
2023-12-31 10:29:15 +0100

    t

---
+++ src/Component/Traditions.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+const Traditions = () => {
+ 
+
+  return (
+    <div className='container-traditions'>
+
+    </div>
+  );
+};
+
+export default Traditions;
